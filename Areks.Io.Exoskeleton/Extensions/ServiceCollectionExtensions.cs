d0296f4820cea97c966561f34170dae56cf66ae2
CallMeAreks <alejandroariel.gc@gmail.com>
2021-03-11 03:53:15 -0600

    Improved the filtering for Configure method renamed the helper methods

--- Areks.Io.Exoskeleton/Extensions/ServiceCollectionExtensions.cs
+++ Areks.Io.Exoskeleton/Extensions/ServiceCollectionExtensions.cs
@@ -21,31 +21,25 @@ namespace Areks.Io.Exoskeleton.Extensions
 
         private static List<Type> GetSettingTypes()
         {
-            return typeof(Startup).Assembly.ExportedTypes
-                .Where(IsSettingSection)
-                .ToList();
+            return typeof(Startup).Assembly.ExportedTypes.Where(TypeInheritsBaseSettings).ToList();
         }
 
-        private static bool IsSettingSection(Type type) => MatchesType(typeof(BaseSettings), type);
+        private static bool TypeInheritsBaseSettings(Type type) => typeof(BaseSettings).InheritsType(type);
         
-        private static bool MatchesType(Type objectType, Type targetType)
+        private static bool InheritsType(this Type objectType, Type targetType)
         {
             return objectType.IsAssignableFrom(targetType) 
                    && targetType.IsClass
+                   && !targetType.IsInterface
                    && !targetType.IsAbstract;
         }
         
-        private static IServiceCollection Configure(this IServiceCollection services, Type type, IConfiguration configuration)
+        private static void Configure(this IServiceCollection services, Type type, IConfiguration configuration)
         {
             // Static type that contains the extension method
             var extMethodType = typeof(OptionsConfigurationServiceCollectionExtensions);
-
-            // IConfiguration config
             
-            // Find the overload for Configure<TOptions>(IServiceCollection, Action<TOptions>)
-            // This could be more specific to make sure that all type arguments are exactly correct.
-            // As it stands, this returns the correct overload but future updates to OptionsServiceCollectionExtensions
-            // may add additional overloads which will require this to be updated.
+            // Find the overload for Configure<TOptions>(IServiceCollection, IConfiguration)
             var genericConfigureMethodInfo = extMethodType.GetMethods()
                 .Where(m => m.IsGenericMethod && m.Name == "Configure")
                 .Select(m => new
@@ -55,18 +49,15 @@ namespace Areks.Io.Exoskeleton.Extensions
                     Args = m.GetGenericArguments() // Generic Type[] (ex [TOptions])
                 })
                 .Where(m => m.Args.Length == 1 && m.Params.Length == 2
-                                               && m.Params[0].ParameterType == typeof(IServiceCollection))
+                                               && m.Params[0].ParameterType == typeof(IServiceCollection)
+                                               && m.Params[1].ParameterType == typeof(IConfiguration))
                 .Select(m => m.Method)
                 .Single();
 
             var method = genericConfigureMethodInfo.MakeGenericMethod(type);
 
-            // Invoke the method via reflection with our converted Action<objct> delegate
-            // Since this is an extension method, it is static and services is passed
-            // as the first parameter instead of the target object
+            // Invoke the method via reflection with the IServiceCollection and IConfiguration objects
             method.Invoke(null, new object[] { services, configuration });
-
-            return services;
         }
     }
 }
